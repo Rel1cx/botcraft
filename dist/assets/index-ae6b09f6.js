d4d53ded37d5b736dc34bb53beefbcba6db0817b
Eva1ent <let@ik.me>
2023-06-18 09:55:17 +0800

    Merge multiple-bots

--- dist/assets/index-ae6b09f6.js
+++
@@ -1,1 +0,0 @@
-const e={ABOUT:"About",DARK:"Dark",GENERAL:"General",INTERFACE:"Interface",LANGUAGE:"Language",LIGHT:"Light",PREFERENCES:"Preferences",QUIT:"Quit",RESET:"Reset",START_AT_LOGIN:"Start at login",THEME:"Theme",VERSION:"Version"};export{e as default};
