ff818c8ae2c995dd2dfa87e7ac07b89c6854f079
Eva1ent <let@ik.me>
2023-06-19 01:26:51 +0800

    style: remove unnecessary imports and code

--- src/pages/BotArea/BotArea.tsx
+++ src/pages/BotArea/BotArea.tsx
@@ -1,12 +1,8 @@
-import { ActionIcon } from "@mantine/core"
-import { useDisclosure } from "@mantine/hooks"
-import { Gear } from "@phosphor-icons/react"
 import { useAtomValue } from "jotai"
 import { Component, lazy, Suspense, useMemo } from "react"
 import { match } from "ts-pattern"
 
 import { addChatAtom, botAtom, botsAtom, botsStore, sortedChatsAtom } from "@/atoms"
-import Icon from "@/components/atoms/Icon/Icon"
 import Redirect from "@/components/atoms/Redirect/Redirect"
 import { BotList } from "@/components/BotList/BotList"
 import { isStampID } from "@/lib/uuid"
@@ -58,7 +54,6 @@ const BotArea = ({ botName }: BotProps) => {
     const route = Router.useRoute(["BotRoot", "BotChat", "BotNewChat", "BotSettings", "BotChatArchive"])
     // const hasApiKey = !!useAtomValue(apiKeyAtom)
     const bots = useAtomValue(botsAtom)
-    const [opened, { close, open }] = useDisclosure(false)
 
     const contentView = useMemo(
         () =>
@@ -83,14 +78,7 @@ const BotArea = ({ botName }: BotProps) => {
     )
 
     return (
-        <RootLayout
-            nav={<BotList items={bots} selected={botName} />}
-            navFooter={
-                <ActionIcon onClick={open} aria-label="Settings" title="Settings">
-                    <Icon as={Gear} />
-                </ActionIcon>
-            }
-        >
+        <RootLayout nav={<BotList items={bots} selected={botName} />}>
             <Suspense>{contentView}</Suspense>
         </RootLayout>
     )
