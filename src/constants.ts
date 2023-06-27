5cb3b7a1c0fd459ae196c31b051db9aaffe2a949
Eva1ent <let@ik.me>
2023-06-27 16:17:34 +0800

    style: update chat title completion prompt

--- src/constants.ts
+++ src/constants.ts
@@ -26,5 +26,5 @@ export const DEFAULT_SYSTEM_MESSAGE = stripIndentTrim(`
 export const DEFAULT_CHAT_TITLE_COMPLETION_PROMPT: Record<Locales, string> = {
     "zh-CN": "为本次聊天生成一个简短的标题\n只回复标题, 不要包含除标题外的任何其他文本",
     // eslint-disable-next-line max-len
-    en: "Generate a brief title for this chat\nJust reply with the title, don't include any other text besides the title",
+    en: "Generate a short title for this chat\nJust reply with the title, don't include any other text besides the title",
 }
