88e95f59a50caff89b2ec20bafb21665bb3fd479
Eva1ent <let@ik.me>
2023-07-05 21:01:00 +0800

    chore: refactor data initialization into a separate file
    - Move initial data for bots, chats, and messages to a separate file `initials.ts`
    - Import the initial data from `initials.ts` in `db.ts`
    
    Signed-off-by: Eva1ent <let@ik.me>

--- src/stores/db.ts
+++ src/stores/db.ts
@@ -1,48 +1,28 @@
 import type { Option as O } from "@swan-io/boxed"
 import { MiniDb } from "jotai-minidb"
-import { omit } from "rambda"
 
-import { defaultBot, initChat } from "@/bot"
 import type { Bot, MessageData } from "@/bot/types"
 import type { ChatItem } from "@/types"
 import type { MessageID } from "@/zod/id"
 
-export const initialChat = initChat()(defaultBot)
-
-export const initialBots: Record<string, Bot> = {
-    [defaultBot.name]: {
-        ...defaultBot,
-        chats: [initialChat.id],
-    },
-}
-
-export const initialChats: Record<string, ChatItem> = {
-    [initialChat.id]: {
-        ...omit(["content"], initialChat),
-        messages: initialChat.content.map((message) => message.id),
-    },
-}
-
-export const initialMessages: Record<string, MessageData> = Object.fromEntries(
-    initialChat.content.map((message) => [message.id, message]),
-)
+import { INITIAL_BOTS, INITIAL_CHATS, INITIAL_MESSAGES } from "./initials"
 
 export const botsDb = new MiniDb<Bot>({
     name: "bots",
     version: 0,
-    initialData: initialBots,
+    initialData: INITIAL_BOTS,
 })
 
 export const chatsDb = new MiniDb<ChatItem>({
     name: "chats",
     version: 0,
-    initialData: initialChats,
+    initialData: INITIAL_CHATS,
 })
 
 export const messagesDb = new MiniDb<MessageData>({
     name: "messages",
     version: 0,
-    initialData: initialMessages,
+    initialData: INITIAL_MESSAGES,
 })
 
 export const draftsDb = new MiniDb<{
