9207b3e7bf0aa7518a6ffc152b761e55eb71cbb2
Eva1ent <let@ik.me>
2023-07-12 16:33:06 +0800

    chore: update scripts and package.json for enhanced functionality
    - Modify `package.json` to include additional paths for `leasot` command in the `todo` script
    - Add `typecheck` script to `check.ts` file in the `scripts` directory
    
    Signed-off-by: Eva1ent <let@ik.me>

--- scripts/check.ts
+++ scripts/check.ts
@@ -3,6 +3,7 @@ import { $ } from "zx";
 await $`pnpm run i18n`;
 
 await Promise.allSettled([
+    $`pnpm run typecheck`,
     $`pnpm rome format .`,
     $`pnpm rome check .`,
     $`pnpm tsc --noEmit`,
